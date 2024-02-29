e88c666a2ebbdb5f3e7da384faed5a2c3b3b818e
Afonso <afonsopro.business@gmail.com>
2024-02-29 10:10:42 +0000

    wpf first tutorial

---
+++ TutorialWPF/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace TutorialWPF
+{
+    
+    public partial class MainWindow : Window
+    {
+        bool running = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (running)
+            {
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }else
+            {
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+            running = !running;
+        }
+    }
+}
